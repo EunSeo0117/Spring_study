8e89a0b94ba2020be33bf47f6071a1317c6292fe
EunSeo0117 <gmjea010@naver.com>
2024-05-15 03:59:35 +0900

    2024.05.15

--- upload/src/main/java/hello/upload/file/FileStore.java
+++ upload/src/main/java/hello/upload/file/FileStore.java
@@ -1,25 +1,23 @@
 package hello.upload.file;
 
 import hello.upload.domain.UploadFile;
+import hello.upload.domain.UploadFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 @Component
 public class FileStore {
     @Value("${file.dir}")
     private String fileDir;
-
     public String getFullPath(String filename) {
         return fileDir + filename;
     }
-
     public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
         List<UploadFile> storeFileResult = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFiles) {
@@ -27,60 +25,29 @@ public class FileStore {
                 storeFileResult.add(storeFile(multipartFile));
             }
         }
-        return storeFileResult; //16:!6분
+        return storeFileResult;
     }
-
-    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException
+    {
         if (multipartFile.isEmpty()) {
             return null;
         }
-
         String originalFilename = multipartFile.getOriginalFilename();
         String storeFileName = createStoreFileName(originalFilename);
         multipartFile.transferTo(new File(getFullPath(storeFileName)));
-
         return new UploadFile(originalFilename, storeFileName);
     }
-
     private String createStoreFileName(String originalFilename) {
         String ext = extractExt(originalFilename);
         String uuid = UUID.randomUUID().toString();
         return uuid + "." + ext;
     }
-
     private String extractExt(String originalFilename) {
-        int pos = originalFilename.lastIndexOf(",");
+        int pos = originalFilename.lastIndexOf(".");
         return originalFilename.substring(pos + 1);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
