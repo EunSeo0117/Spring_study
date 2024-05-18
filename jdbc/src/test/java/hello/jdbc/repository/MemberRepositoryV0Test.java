73257849dd4082a998d3ba2bd7fa7bebfa931032
EunSeo0117 <gmjea010@naver.com>
2024-05-19 04:17:39 +0900

    2024.05.19

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
@@ -15,13 +17,38 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV7", 10000);
         repository.save(member);
 
         //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         Assertions.assertThat(findMember).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(()-> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
